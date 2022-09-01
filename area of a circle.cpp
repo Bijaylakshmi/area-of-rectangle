b8df4792ceeb6b7c62510a516b41002166534a58
Bijaylakshmi Kumari <112059431+Bijaylakshmi@users.noreply.github.com>
2022-09-01 15:20:41 +0530

    area of cc

---
+++ area of a circle.cpp
@@ -0,0 +1,21 @@
+//area of a rectangle
+#include<iostream>
+using namespace std;
+class volume
+{
+		int l, b, h;
+		public:
+		volume()
+		{
+		l=5, b=6, h=7;
+	}
+	void a()
+	{
+		cout<<l*b*h;
+	   }
+	};
+	main()
+	{
+		 class volume obj;
+		 obj.a();
+	}
